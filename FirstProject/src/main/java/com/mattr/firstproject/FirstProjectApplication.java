c723b0469b59a4e03a9f37c5619d1bd00ed5a3b1
mateogolf <rodriguezml11@mail.wlu.edu>
2017-09-08 12:30:44 -0400

    Initial Commit and first project notes

---
+++ FirstProject/src/main/java/com/mattr/firstproject/FirstProjectApplication.java
@@ -0,0 +1,27 @@
+package com.mattr.firstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+//	@RequestMapping("/")
+//	public String hello() {
+//		return "Hello client! How are you doing";
+//	}
+//	@RequestMapping("/random")
+//	public String random() {
+//		return "Spring is great! So easy to just respond with strings";
+//	}
+}
