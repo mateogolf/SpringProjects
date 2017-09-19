08ab848981959078b034024b1f703f644a3fab5d
mateogolf <rodriguezml11@mail.wlu.edu>
2017-09-19 17:35:34 -0400

    testing advanced queries

--- dojoninjas/src/main/java/com/mattr/dojoninjas/repositories/DojoRepo.java
+++ dojoninjas/src/main/java/com/mattr/dojoninjas/repositories/DojoRepo.java
@@ -1,10 +1,14 @@
 package com.mattr.dojoninjas.repositories;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.mattr.dojoninjas.models.Dojo;
 @Repository
 public interface DojoRepo extends CrudRepository<Dojo,Long>{
-
+	@Query(value="SELECT *,COUNT(ninja.id) As ninjaCount FROM dojo INNER JOIN dojo.id=ninja.dojo ORDER BY ninjaCount DESC",nativeQuery=true)
+	ArrayList<Dojo> findAllDojosByNinjaCountDesc();
 }
